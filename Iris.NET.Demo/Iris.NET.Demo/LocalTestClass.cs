27b5fa9b56de132c556297e29dde1166f87a4478
Tommaso Bertoni <bertonitommaso@gmail.com>
2017-03-05 22:40:30 +0100

    Updated Demo project for beta-2, updated assemblies version (0.2.0.0)

--- Iris.NET.Demo/Iris.NET.Demo/LocalTestClass.cs
+++ Iris.NET.Demo/Iris.NET.Demo/LocalTestClass.cs
@@ -5,16 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Iris.NET.Demo
 {
     class LocalTestClass
     {
         private IrisServerConfig _config = new IrisServerConfig(new IrisPubSubRouter());
-        private IrisServerLocalNode localNode1 = new IrisServerLocalNode();
+        private IrisLocalNode localNode1 = new IrisLocalNode();
 
-        private IrisServerLocalNode localNode2 = new IrisServerLocalNode();
-        private IrisServerLocalNode localNode3 = new IrisServerLocalNode();
+        private IrisLocalNode localNode2 = new IrisLocalNode();
+        private IrisLocalNode localNode3 = new IrisLocalNode();
         private List<IDisposableSubscription> _disposableSubscriptions = new List<IDisposableSubscription>();
 
         private string _channel = "chatroom";
@@ -62,20 +63,21 @@ namespace Iris.NET.Demo
             // The content is the data that is sent by the nodes, the IrisContextHook is an internal managed object that gives context
             // information to the handler about the content and the subscription state.
             // GenericContentHandler is a local method that simply prints to the console the content in a pretty and standard way.
-            var subscription1 = localNode1.SubscribeToBroadcast((c, h) => GenericContentHandler(nameof(localNode1), "broadcast", c, h));
+            Task<IDisposableSubscription> subscription1 = localNode1.SubscribeToBroadcast((c, h) => GenericContentHandler(nameof(localNode1), "broadcast", c, h));
             bool result1 = subscription1 != null;
-            var subscription2 = localNode2.SubscribeToBroadcast((c, h) => GenericContentHandler(nameof(localNode2), "broadcast", c, h));
+            Task<IDisposableSubscription> subscription2 = localNode2.SubscribeToBroadcast((c, h) => GenericContentHandler(nameof(localNode2), "broadcast", c, h));
             bool result2 = subscription2 != null;
 
             CheckThatEverythingIsOk(result1, result2);
-            disposableSubscriptions.Add(subscription1);
-            disposableSubscriptions.Add(subscription2);
+            disposableSubscriptions.Add(subscription1.Result);
+            disposableSubscriptions.Add(subscription2.Result);
             Console.WriteLine($"The nodes {nameof(localNode1)}, {nameof(localNode2)} are subscribed to the broadcast.");
 
             string message = "Hello!";
             Console.WriteLine($"{nameof(localNode1)} sends \"{message}\" in broadcast.");
             // The message will be received by all the nodes subscribed to the broadcast, except the sender.
-            result1 = localNode1.SendToBroadcast(message);
+            Task<bool> publishToBroadcastTask = localNode1.PublishToBroadcast(message);
+            result1 = publishToBroadcastTask.Result;
 
             CheckThatEverythingIsOk(result1);
             return disposableSubscriptions;
@@ -87,24 +89,25 @@ namespace Iris.NET.Demo
 
             // In this test, subscription1 is actually useless because localNode1 is sending the messages, so it won't receive any content.
             // Note that a subscription to a channel is not needed to send a content to that channel, and the same goes for the broadcast.
-            var subscription1 = localNode1.Subscribe(_channel, (c, h) => GenericContentHandler(nameof(localNode1), _channel, c, h));
+            Task<IDisposableSubscription> subscription1 = localNode1.Subscribe(_channel, (c, h) => GenericContentHandler(nameof(localNode1), _channel, c, h));
             bool result1 = subscription1 != null;
 
-            var subscription2 = localNode2.Subscribe(_channel, (c, h) => GenericContentHandler(nameof(localNode2), _channel, c, h));
+            Task<IDisposableSubscription> subscription2 = localNode2.Subscribe(_channel, (c, h) => GenericContentHandler(nameof(localNode2), _channel, c, h));
             bool result2 = subscription2 != null;
 
-            var subscription3 = localNode3.Subscribe(_channel, (c, h) => GenericContentHandler(nameof(localNode3), _channel, c, h));
+            Task<IDisposableSubscription> subscription3 = localNode3.Subscribe(_channel, (c, h) => GenericContentHandler(nameof(localNode3), _channel, c, h));
             bool result3 = subscription3 != null;
 
             CheckThatEverythingIsOk(result1, result2, result3);
             Console.WriteLine($"The nodes {nameof(localNode1)}, {nameof(localNode2)}, {nameof(localNode3)} are subscribed to the \"{_channel}\" channel.");
-            disposableSubscriptions.Add(subscription1);
-            disposableSubscriptions.Add(subscription2);
-            disposableSubscriptions.Add(subscription3);
+            disposableSubscriptions.Add(subscription1.Result);
+            disposableSubscriptions.Add(subscription2.Result);
+            disposableSubscriptions.Add(subscription3.Result);
 
             string message = $"Hi! my name is {nameof(localNode1)}";
             Console.WriteLine($"{nameof(localNode1)} sends \"{message}\" to the \"{_channel}\" channel.");
-            result1 = localNode1.Send(_channel, message);
+            Task<bool> publishTask1 = localNode1.Publish(_channel, message);
+            result1 = publishTask1.Result;
             CheckThatEverythingIsOk(result1);
 
             WaitALittle();
@@ -113,7 +116,8 @@ namespace Iris.NET.Demo
             message = $"{message} [in broadcast]";
             Console.WriteLine($"{nameof(localNode1)} sends \"{message}\" in broadcast.");
             Console.WriteLine($"// Notice that {nameof(localNode3)} won't receive the message because it isn't subscribed to the broadcast communication.");
-            result1 = localNode1.SendToBroadcast(message);
+            Task<bool> publishToBroadcastTask = localNode1.PublishToBroadcast(message);
+            result1 = publishToBroadcastTask.Result;
             CheckThatEverythingIsOk(result1);
 
             return disposableSubscriptions;
@@ -129,26 +133,28 @@ namespace Iris.NET.Demo
 
             string message = $"Hey, I'm {nameof(localNode1)}. Those who want to talk about programming head over to the {nestedChannelHierarchy} channel!";
             Console.WriteLine($"{nameof(localNode1)} sends \"{message}\" to the \"{_channel}\" channel.");
-            bool result1 = localNode1.Send(_channel, message);
+            Task<bool> publishTask = localNode1.Publish(_channel, message);
+            bool result1 = publishTask.Result;
             CheckThatEverythingIsOk(result1);
 
             WaitALittle();
             Console.WriteLine();
 
-            var subscription1 = localNode1.Subscribe($"{nestedChannelHierarchy}", (c, h) => GenericContentHandler(nameof(localNode1), nestedChannelHierarchy, c, h));
+            Task<IDisposableSubscription> subscription1 = localNode1.Subscribe($"{nestedChannelHierarchy}", (c, h) => GenericContentHandler(nameof(localNode1), nestedChannelHierarchy, c, h));
             result1 = subscription1 != null;
 
-            var subscription3 = localNode3.Subscribe($"{nestedChannelHierarchy}", (c, h) => GenericContentHandler(nameof(localNode3), nestedChannelHierarchy, c, h));
+            Task<IDisposableSubscription> subscription3 = localNode3.Subscribe($"{nestedChannelHierarchy}", (c, h) => GenericContentHandler(nameof(localNode3), nestedChannelHierarchy, c, h));
             bool result3 = subscription3 != null;
 
             CheckThatEverythingIsOk(result1, result3);
             Console.WriteLine($"The nodes {nameof(localNode1)}, {nameof(localNode3)} are subscribed to the \"{nestedChannelHierarchy}\" channel.");
-            disposableSubscriptions.Add(subscription1);
-            disposableSubscriptions.Add(subscription3);
+            disposableSubscriptions.Add(subscription1.Result);
+            disposableSubscriptions.Add(subscription3.Result);
 
             message = "Let's talk about programming!";
             Console.WriteLine($"{nameof(localNode1)} sends \"{message}\" to the \"{nestedChannelHierarchy}\" channel.");
-            result1 = localNode1.Send(nestedChannelHierarchy, message);
+            publishTask = localNode1.Publish(nestedChannelHierarchy, message);
+            result1 = publishTask.Result;
             CheckThatEverythingIsOk(result1);
 
             return disposableSubscriptions;
@@ -160,7 +166,8 @@ namespace Iris.NET.Demo
             Console.WriteLine($"{nameof(localNode1)} sends \"{message}\" to the \"{_channel}\" channel and all its subchannels.");
             // By setting the "propagateThroughHierarchy" parameter to "true" the content will be sent to every content handler in the target channel
             // and to every content handler in all the child channels, recursively.
-            bool result1 = localNode1.Send(_channel, message, propagateThroughHierarchy: true);
+            Task<bool> publishTask = localNode1.Publish(_channel, message, propagateThroughHierarchy: true);
+            bool result1 = publishTask.Result;
             CheckThatEverythingIsOk(result1);
         }
 
@@ -168,26 +175,27 @@ namespace Iris.NET.Demo
         {
             List<IDisposableSubscription> disposableSubscriptions = new List<IDisposableSubscription>();
 
-            var subscription1 = localNode1.Subscribe($"{localNode1.Id.ToString()}", (c, h) => GenericContentHandler(nameof(localNode1), localNode1.Id.ToString(), c, h));
+            Task<IDisposableSubscription> subscription1 = localNode1.Subscribe($"{localNode1.Id.ToString()}", (c, h) => GenericContentHandler(nameof(localNode1), localNode1.Id.ToString(), c, h));
             bool result1 = subscription1 != null;
 
-            var subscription2 = localNode2.Subscribe($"{localNode2.Id.ToString()}", (c, h) => GenericContentHandler(nameof(localNode2), localNode2.Id.ToString(), c, h));
+            Task<IDisposableSubscription> subscription2 = localNode2.Subscribe($"{localNode2.Id.ToString()}", (c, h) => GenericContentHandler(nameof(localNode2), localNode2.Id.ToString(), c, h));
             bool result2 = subscription2 != null;
 
-            var subscription3 = localNode3.Subscribe($"{localNode3.Id.ToString()}", (c, h) => GenericContentHandler(nameof(localNode3), localNode3.Id.ToString(), c, h));
+            Task<IDisposableSubscription> subscription3 = localNode3.Subscribe($"{localNode3.Id.ToString()}", (c, h) => GenericContentHandler(nameof(localNode3), localNode3.Id.ToString(), c, h));
             bool result3 = subscription3 != null;
 
             CheckThatEverythingIsOk(result1, result2, result3);
             Console.WriteLine($"The nodes {nameof(localNode1)}, {nameof(localNode2)}, {nameof(localNode3)} are subscribed to their \"personal\" channel (based on their Id).");
-            disposableSubscriptions.Add(subscription1);
-            disposableSubscriptions.Add(subscription2);
-            disposableSubscriptions.Add(subscription3);
+            disposableSubscriptions.Add(subscription1.Result);
+            disposableSubscriptions.Add(subscription2.Result);
+            disposableSubscriptions.Add(subscription3.Result);
 
             // Until now the test only sent strings, but the content expected by the Send method is actually of type object.
 
             double d = 0.42;
             Console.WriteLine($"{nameof(localNode1)} sends \"{d}\" to the {nameof(localNode2)} personal channel.");
-            result1 = localNode1.Send(localNode2.Id.ToString(), d);
+            Task<bool> publishTask = localNode1.Publish(localNode2.Id.ToString(), d);
+            result1 = publishTask.Result;
             CheckThatEverythingIsOk(result1);
 
             WaitALittle();
@@ -195,7 +203,8 @@ namespace Iris.NET.Demo
 
             var obj = new TestType($"\"A present for {nameof(localNode3)}\"") { Count = 42 };
             Console.WriteLine($"{nameof(localNode2)} sends \"{obj}\" to the {nameof(localNode3)} personal channel.");
-            result2 = localNode2.Send(localNode3.Id.ToString(), obj);
+            publishTask = localNode2.Publish(localNode3.Id.ToString(), obj);
+            result2 = publishTask.Result;
             CheckThatEverythingIsOk(result2);
 
             return disposableSubscriptions;
@@ -206,14 +215,17 @@ namespace Iris.NET.Demo
             // To remove a subscription you can use the Unsubscribe method, or dispose the IDisposableSubscription returned by the Subscribe method.
 
             _disposableSubscriptions.ForEach(ds => ds.Dispose());
+
+            WaitALittle();
             Console.WriteLine("All the subscriptions have been disposed.");
+
             if (_IDontBelieveIt)
             {
                 Console.WriteLine("You don't believe it? Let's send a couple of messages...");
                 var message = "Super-important-communication-that-cannot-be-ignored";
-                localNode1.SendToBroadcast(message);
-                localNode1.Send(_channel, message);
-                localNode1.Send($"{_channel}/{_nestedChannelName}", message);
+                localNode1.PublishToBroadcast(message);
+                localNode1.Publish(_channel, message);
+                localNode1.Publish($"{_channel}/{_nestedChannelName}", message);
                 WaitALittle();
                 Console.WriteLine("Nobody should have received any message.");
             }
@@ -249,7 +261,10 @@ namespace Iris.NET.Demo
 
         private void GenericContentHandler(string receiverName, string subscribedChannel, object content, IrisContextHook hook)
         {
-            Console.WriteLine($"{receiverName} received \"{content.ToString()}\" from \"{subscribedChannel}\"");
+            if (hook.Unsubscribing)
+                Console.WriteLine("Unsubscribing!");
+            else
+                Console.WriteLine($"{receiverName} received \"{content?.ToString()}\" from \"{subscribedChannel}\"");
         }
 
         private void CheckThatEverythingIsOk(params bool[] results)
@@ -262,9 +277,9 @@ namespace Iris.NET.Demo
             }
         }
 
-        private void WaitALittle()
+        private void WaitALittle(int delay = 300)
         {
-            Thread.Sleep(300); // Gives some time to the asyncronous operations to complete
+            Thread.Sleep(delay); // Gives some time to the asyncronous operations to complete
         }
         #endregion
     }
